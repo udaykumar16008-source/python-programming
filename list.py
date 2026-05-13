a4a29630daa5ddc3e01cf253ae0a5925c08f4198
udaykumar <udaykumar16008@gmail.com>
2026-05-13 10:54:46 +0530

    Add files via upload
    Pets are : ['cat', 'dog', 'rat', 'pig', 'tiger']
    Snakes are : ['python', 'anaconda', 'fish', 'cobra', 'mamba']
    Animals are : ['cat', 'dog', 'rat', 'pig', 'tiger', 'python', 'anaconda', 'fish', 'cobra', 'mamba']
    updated Snakes are : ['python', 'anaconda', 'cobra', 'mamba']

---
+++ list.py
@@ -0,0 +1,8 @@
+pets = ['cat', 'dog', 'rat', 'pig', 'tiger']
+snakes=['python','anaconda','fish','cobra','mamba']
+print("Pets are :",pets)
+print("Snakes are :",snakes)
+animals=pets+snakes
+print("Animals are :",animals)
+snakes.remove("fish")
+print("updated Snakes are :",snakes) 
